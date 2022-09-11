6b22b0617b450e094113f845e4ada27194cd9367
DELL <fahimbuet1995@gmail.com>
2022-09-11 17:05:23 +0600

    - Filter Show/Hide option added in   - Machine Wise & Root Cause wise Count View

--- src/report/MachineWiseRootCauseCountReport.js
+++ src/report/MachineWiseRootCauseCountReport.js
@@ -4,6 +4,7 @@ import Pagination from "@mui/material/Pagination";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
+import FilterIcon from "@mui/icons-material/FilterList";
 
 export default class MachineWiseRootCauseCountReport extends Component{
     constructor(props) {
@@ -12,7 +13,8 @@ export default class MachineWiseRootCauseCountReport extends Component{
             machineWiseRootCauseCountData:[],
             currentPage:1, totalPages:1, pageSize:10,recordCount:1,
             division:'',region:'',territory:'',machineBrand:'',machineModel:'',rootCause:'',
-            machineNumber:'',startDate:'',endDate:'',countStart:'',countEnd:''
+            machineNumber:'',startDate:'',endDate:'',countStart:'',countEnd:'',
+            filterBoxVisible:false
         }
         this.machineWiseRootCauseCountApi = '/api/v1/issue/report/machine_wise_root_cause_count';
     }
@@ -47,103 +49,118 @@ export default class MachineWiseRootCauseCountReport extends Component{
 
     render() {
         return <div>
-            <h4>Machine Wise Root Cause Frequency</h4>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Division" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({division:e.target.value})}}
-                                   value={this.state.division}/>
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Region" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({region:e.target.value})}}
-                                   value={this.state.region}/>
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Territory" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({territory:e.target.value})}}
-                                   value={this.state.territory}/>
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Machine Brand" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineBrand:e.target.value})}}
-                                   value={this.state.machineBrand}/>
-                    </div>
+            <div className='row'>
+                <div className='col-lg-10'>
+                    <h4>Machine Wise Root Cause Frequency</h4>
                 </div>
-                <div className='row'>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Machine Model" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineModel:e.target.value})}}
-                                   value={this.state.machineModel}/>
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Machine Number" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineNumber:e.target.value})}}
-                                   value={this.state.machineNumber}/>
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Start Date" variant="standard" fullWidth
-                                   InputLabelProps={{ shrink: true }}
-                                   type="date" value={this.state.startDate}
-                                   onChange={(e)=>{this.setState({startDate:e.target.value})}}
-                        />
-                    </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="End Date" variant="standard" fullWidth
-                                   InputLabelProps={{ shrink: true }}
-                                   type="date" value={this.state.endDate}
-                                   onChange={(e)=>{this.setState({endDate:e.target.value})}}
-                        />
-                    </div>
+                <div className='col-lg-2'>
+                    <Button fullWidth variant="contained" endIcon={<FilterIcon />}
+                            onClick={(e)=>{this.setState({filterBoxVisible:!this.state.filterBoxVisible})}}
+                    >Filter
+                    </Button>
                 </div>
-                <div className="row">
-                    <div className='col-sm-3 p-3'>
-                        <label><b>Frequency Range:</b></label>
+            </div>
+            {
+                this.state.filterBoxVisible?
+                    <div className='container'>
                         <div className='row'>
-                            <div className='col-lg-6'>
-                                <TextField label="Start" variant="standard" fullWidth
-                                           type='number' value={this.state.countStart}
-                                           onChange={(e) => {this.setState({countStart:e.target.value})}}
-                                           />
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Division" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({division:e.target.value})}}
+                                           value={this.state.division}/>
                             </div>
-                            <div className='col-lg-6'>
-                                <TextField label="End" variant="standard" fullWidth
-                                           type='number' value={this.state.countEnd}
-                                           onChange={(e) => {this.setState({countEnd:e.target.value})}}
-                                           />
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Region" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({region:e.target.value})}}
+                                           value={this.state.region}/>
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Territory" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({territory:e.target.value})}}
+                                           value={this.state.territory}/>
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Machine Brand" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineBrand:e.target.value})}}
+                                           value={this.state.machineBrand}/>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Machine Model" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineModel:e.target.value})}}
+                                           value={this.state.machineModel}/>
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Machine Number" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineNumber:e.target.value})}}
+                                           value={this.state.machineNumber}/>
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Start Date" variant="standard" fullWidth
+                                           InputLabelProps={{ shrink: true }}
+                                           type="date" value={this.state.startDate}
+                                           onChange={(e)=>{this.setState({startDate:e.target.value})}}
+                                />
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="End Date" variant="standard" fullWidth
+                                           InputLabelProps={{ shrink: true }}
+                                           type="date" value={this.state.endDate}
+                                           onChange={(e)=>{this.setState({endDate:e.target.value})}}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className='col-sm-3 p-3'>
+                                <label><b>Frequency Range:</b></label>
+                                <div className='row'>
+                                    <div className='col-lg-6'>
+                                        <TextField label="Start" variant="standard" fullWidth
+                                                   type='number' value={this.state.countStart}
+                                                   onChange={(e) => {this.setState({countStart:e.target.value})}}
+                                        />
+                                    </div>
+                                    <div className='col-lg-6'>
+                                        <TextField label="End" variant="standard" fullWidth
+                                                   type='number' value={this.state.countEnd}
+                                                   onChange={(e) => {this.setState({countEnd:e.target.value})}}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-sm-3 p-3'>
+                                <TextField label="Root Cause" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({rootCause:e.target.value})}}
+                                           value={this.state.rootCause}/>
+                            </div>
+                            <div className="col-lg-5"> </div>
+                            <div className="col-lg-1 p-3">
+                                <Button  onClick={(e)=>{
+                                    this.setState({currentPage:1},()=>{
+                                        this.componentDidMount();
+                                    });}}
+                                         variant="contained" endIcon={<SearchIcon />}> </Button>
                             </div>
                         </div>
                     </div>
-                    <div className='col-sm-3 p-3'>
-                        <TextField label="Root Cause" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({rootCause:e.target.value})}}
-                                   value={this.state.rootCause}/>
-                    </div>
-                    <div className="col-lg-5"> </div>
-                    <div className="col-lg-1 p-3">
-                        <Button  onClick={(e)=>{
-                            this.setState({currentPage:1},()=>{
-                                this.componentDidMount();
-                            });}}
-                                variant="contained" endIcon={<SearchIcon />}> </Button>
-                    </div>
+                    :''
+            }
+
+            <div className="row">
+                <div className="col-lg-3 my-auto p-3">
+                    <label><b>
+                        Showing {(this.state.currentPage-1)*this.state.pageSize+1}&nbsp;-&nbsp;
+                        {(this.state.currentPage-1)*this.state.pageSize+this.state.machineWiseRootCauseCountData.length}
+                        &nbsp;of&nbsp;{this.state.recordCount}
+                    </b></label>
                 </div>
-                <div className="row">
-                    <div className="col-lg-3 my-auto p-3">
-                        <label><b>
-                            Showing {(this.state.currentPage-1)*this.state.pageSize+1}&nbsp;-&nbsp;
-                            {(this.state.currentPage-1)*this.state.pageSize+this.state.machineWiseRootCauseCountData.length}
-                            &nbsp;of&nbsp;{this.state.recordCount}
-                        </b></label>
-                    </div>
-                    <div className="col-lg-9 p-3" >
-                        <Pagination count={this.state.totalPages} page={this.state.currentPage}
-                                    onChange={(e,val)=>{
-                                        this.setState({currentPage:val},
-                                            ()=>{this.componentDidMount();})}}
-                                    size={"large"}/>
-                    </div>
+                <div className="col-lg-9 p-3" >
+                    <Pagination count={this.state.totalPages} page={this.state.currentPage}
+                                onChange={(e,val)=>{
+                                    this.setState({currentPage:val},
+                                        ()=>{this.componentDidMount();})}}
+                                size={"large"}/>
                 </div>
             </div>
             <div className="row" style={{overflowY:'auto',maxHeight:'550px'}}>
--- src/report/MachineWiseTicketCountReport.js
+++ src/report/MachineWiseTicketCountReport.js
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 import {Link} from "react-router-dom";
 import {withRouter} from "../components/withRouter";
+import FilterIcon from "@mui/icons-material/FilterList";
 
 class MachineWiseTicketCountReport extends Component{
     constructor(props) {
@@ -14,7 +15,8 @@ class MachineWiseTicketCountReport extends Component{
             machineWiseTicketCountData:[],
             currentPage:1, totalPages:1, pageSize:10,recordCount:1,
             division:'',region:'',territory:'',machineBrand:'',machineModel:'',
-            machineNumber:'',startDate:'',endDate:'',countStart:'',countEnd:''
+            machineNumber:'',startDate:'',endDate:'',countStart:'',countEnd:'',
+            filterBoxVisible:false
         }
         this.machineWiseTicketCountApi = '/api/v1/issue/report/machine_wise_ticket_count';
     }
@@ -47,97 +49,111 @@ class MachineWiseTicketCountReport extends Component{
 
     render() {
         return <div>
-            <h4>Machine Wise Ticket Frequency</h4>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Division" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({division:e.target.value})}}
-                                   value={this.state.division}/>
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Region" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({region:e.target.value})}}
-                                   value={this.state.region}/>
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Territory" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({territory:e.target.value})}}
-                                   value={this.state.territory}/>
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Machine Brand" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineBrand:e.target.value})}}
-                                   value={this.state.machineBrand}/>
-                    </div>
+            <div className='row'>
+                <div className='col-lg-10'>
+                    <h4>Machine Wise Ticket Frequency</h4>
                 </div>
-                <div className='row'>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Machine Model" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineModel:e.target.value})}}
-                                   value={this.state.machineModel}/>
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Machine Number" variant="standard" fullWidth
-                                   onChange={(e) => {this.setState({machineNumber:e.target.value})}}
-                                   value={this.state.machineNumber}/>
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="Start Date" variant="standard" fullWidth
-                                   InputLabelProps={{ shrink: true }}
-                                   type="date" value={this.state.startDate}
-                                   onChange={(e)=>{this.setState({startDate:e.target.value})}}
-                        />
-                    </div>
-                    <div className='col-lg-3 p-3'>
-                        <TextField label="End Date" variant="standard" fullWidth
-                                   InputLabelProps={{ shrink: true }}
-                                   type="date" value={this.state.endDate}
-                                   onChange={(e)=>{this.setState({endDate:e.target.value})}}
-                        />
-                    </div>
+                <div className='col-lg-2'>
+                    <Button fullWidth variant="contained" endIcon={<FilterIcon />}
+                            onClick={(e)=>{this.setState({filterBoxVisible:!this.state.filterBoxVisible})}}
+                    >Filter
+                    </Button>
                 </div>
-                <div className="row">
-                    <div className='col-lg-3 p-3'>
-                        <label><b>Frequency Range:</b></label>
+            </div>
+            {
+                this.state.filterBoxVisible?
+                    <div className='container'>
                         <div className='row'>
-                            <div className='col-lg-6'>
-                                <TextField label="Start" variant="standard" fullWidth type='number'
-                                           onChange={(e) => {this.setState({countStart:e.target.value})}}
-                                           value={this.state.countStart}/>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Division" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({division:e.target.value})}}
+                                           value={this.state.division}/>
                             </div>
-                            <div className='col-lg-6'>
-                                <TextField label="End" variant="standard" fullWidth type='number'
-                                           onChange={(e) => {this.setState({countEnd:e.target.value})}}
-                                           value={this.state.countEnd}/>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Region" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({region:e.target.value})}}
+                                           value={this.state.region}/>
+                            </div>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Territory" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({territory:e.target.value})}}
+                                           value={this.state.territory}/>
+                            </div>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Machine Brand" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineBrand:e.target.value})}}
+                                           value={this.state.machineBrand}/>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Machine Model" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineModel:e.target.value})}}
+                                           value={this.state.machineModel}/>
+                            </div>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Machine Number" variant="standard" fullWidth
+                                           onChange={(e) => {this.setState({machineNumber:e.target.value})}}
+                                           value={this.state.machineNumber}/>
+                            </div>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="Start Date" variant="standard" fullWidth
+                                           InputLabelProps={{ shrink: true }}
+                                           type="date" value={this.state.startDate}
+                                           onChange={(e)=>{this.setState({startDate:e.target.value})}}
+                                />
+                            </div>
+                            <div className='col-lg-3 p-3'>
+                                <TextField label="End Date" variant="standard" fullWidth
+                                           InputLabelProps={{ shrink: true }}
+                                           type="date" value={this.state.endDate}
+                                           onChange={(e)=>{this.setState({endDate:e.target.value})}}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className='col-lg-3 p-3'>
+                                <label><b>Frequency Range:</b></label>
+                                <div className='row'>
+                                    <div className='col-lg-6'>
+                                        <TextField label="Start" variant="standard" fullWidth type='number'
+                                                   onChange={(e) => {this.setState({countStart:e.target.value})}}
+                                                   value={this.state.countStart}/>
+                                    </div>
+                                    <div className='col-lg-6'>
+                                        <TextField label="End" variant="standard" fullWidth type='number'
+                                                   onChange={(e) => {this.setState({countEnd:e.target.value})}}
+                                                   value={this.state.countEnd}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-8"> </div>
+                            <div className="col-lg-1 p-3">
+                                <Button fullWidth onClick={(e)=>{
+                                    this.setState({currentPage:1},()=>{
+                                        this.componentDidMount();
+                                    });}}
+                                        variant="contained" endIcon={<SearchIcon />}> </Button>
                             </div>
                         </div>
                     </div>
-
-                    <div className="col-lg-8"> </div>
-                    <div className="col-lg-1 p-3">
-                        <Button fullWidth onClick={(e)=>{
-                            this.setState({currentPage:1},()=>{
-                                this.componentDidMount();
-                            });}}
-                                variant="contained" endIcon={<SearchIcon />}> </Button>
-                    </div>
+                    :''
+            }
+            <div className="row">
+                <div className="col-lg-3 my-auto p-3">
+                    <label><b>
+                        Showing {(this.state.currentPage-1)*this.state.pageSize+1}&nbsp;-&nbsp;
+                        {(this.state.currentPage-1)*this.state.pageSize+this.state.machineWiseTicketCountData.length}
+                        &nbsp;of&nbsp;{this.state.recordCount}
+                    </b></label>
                 </div>
-                <div className="row">
-                    <div className="col-lg-3 my-auto p-3">
-                        <label><b>
-                            Showing {(this.state.currentPage-1)*this.state.pageSize+1}&nbsp;-&nbsp;
-                            {(this.state.currentPage-1)*this.state.pageSize+this.state.machineWiseTicketCountData.length}
-                            &nbsp;of&nbsp;{this.state.recordCount}
-                        </b></label>
-                    </div>
-                    <div className="col-lg-9 p-3" >
-                        <Pagination count={this.state.totalPages} page={this.state.currentPage}
-                                    onChange={(e,val)=>{
-                                        this.setState({currentPage:val},
-                                            ()=>{this.componentDidMount();})}}
-                                    size={"large"}/>
-                    </div>
+                <div className="col-lg-9 p-3" >
+                    <Pagination count={this.state.totalPages} page={this.state.currentPage}
+                                onChange={(e,val)=>{
+                                    this.setState({currentPage:val},
+                                        ()=>{this.componentDidMount();})}}
+                                size={"large"}/>
                 </div>
             </div>
             <div className="row" style={{overflowY:'auto',maxHeight:'550px'}}>
